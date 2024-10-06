a7e4cc431d400d7356883a44a2fc72ea58421ad2
svvelez <rios010503@gmail.com>
2024-10-06 18:46:43 -0500

    add functions crud books

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Books;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
+class BooksController extends Controller
+{
+
+    public function index()
+    {
+        $books = Books::all();
+        return response()->json(['books' => $books]);
+    }
+
+    public function createBook(Request $request){
+        $book = Books::create([
+            'name' => $request->input('name')
+        ]);
+        return response()->json(['success' => 'true']);
+    
+    }
+
+    public function deleteBook(Request $request){
+        $book = Books::find($request->id)->delete();
+        return response()->json(['success' => 'true']);
+    }
+
+    public function getBook(Request $request){
+        $book = Books::find($request->id);
+        return response()->json(['book' => $book ]);
+    }
+
+    public function editBook(Request $request){
+        $book = Books::find($request->id);
+        $book->update([
+            'name' => $request->name
+        ]);
+        return response()->json(['success' => 'true']);
+    }
+
+
+}
